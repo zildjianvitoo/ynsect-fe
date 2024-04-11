f8ee222aff3105c8d3b0ec012e19ddf17d307a70
ZildjianVito <zildjianvito07@gmail.com>
2024-04-11 19:52:11 +0700

    fix: shipment box

--- components/Root/Product/ShipmentBox.tsx
+++ components/Root/Product/ShipmentBox.tsx
@@ -1,13 +1,17 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import { CiEdit } from "react-icons/ci";
 import { addDays, format } from "date-fns";
 import { id } from "date-fns/locale/id";
+import { Input } from "@/components/ui/input";
 
 type Props = {};
 
 export default function ShipmentBox({}: Props) {
+  const [isEdit, setEdit] = useState(false);
+  const [formValue, setFormValue] = useState("Jalan Sudarsono No. 1663");
+
   const today = new Date();
 
   const threeDaysLater = addDays(today, 3);
@@ -25,10 +29,27 @@ export default function ShipmentBox({}: Props) {
       <div className="mt-2 flex items-center justify-between">
         <p className="text-slate-500">Dikirim ke</p>
         <div className="flex items-center gap-2">
-          <h3 className=" font-bold text-[#15514D] ">
-            Jalan Sudarsono No. 1663
-          </h3>
-          <CiEdit size={18} className=" cursor-pointer" />
+          {isEdit ? (
+            <Input
+              className="w-full rounded-md  border bg-transparent  py-2 text-base placeholder:text-base focus:outline-primary focus:ring-primary"
+              placeholder="ex: Jalan sudarsono no 1663"
+              onKeyDown={(e) => {
+                if (e.keyCode === 13) {
+                  const { value } = e.target as HTMLInputElement;
+                  setEdit(false);
+                  setFormValue(value);
+                }
+              }}
+              onBlur={() => setFormValue((prev) => prev)}
+            />
+          ) : (
+            <h3 className=" font-bold text-[#15514D] ">{formValue}</h3>
+          )}
+          <CiEdit
+            size={18}
+            className=" cursor-pointer"
+            onClick={() => setEdit(true)}
+          />
         </div>
       </div>
     </div>
